c116b73358b8cfd7c70dda43e2d894067a79328e
Stanislav Korneev <korstasvik@gmail.com>
2024-04-23 19:29:30 +0200

    added signup and login tabs

---
+++ src/components/LoginView/SignUpTab.vue
@@ -0,0 +1,37 @@
+<template>
+  <slot />
+  <p class="disclaimer">
+    By creating an account you agree to the privacy policy and to receive economic and marketing comunnications from AvaTrade. You can remove yourself from the mailing list at anytime.
+  </p>
+  <p class="partner-promo">
+    Have a partner number?
+    <span class="action-text" @click="handlePartnerPromo">Click here</span>
+  </p>
+</template>
+
+<script setup lang="ts">
+function handlePartnerPromo(): void {
+  // here goes some logic. If it's a link, should change to router-link though
+  console.log('having partner now, do we? ;)')
+}
+</script>
+
+<style scoped>
+.disclaimer {
+  margin-top: 30px;
+  font-size: 12px;
+  text-align: center;
+  color: var(--dark-gray);
+}
+
+.partner-promo {
+  margin-top: 30px;
+  font-size: 16px;
+}
+
+.action-text {
+  color: var(--dark-blue);
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
