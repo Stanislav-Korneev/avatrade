6587b91e8cef45a00a1bf277e9549f503f4cde6f
Stanislav Korneev <korstasvik@gmail.com>
2024-04-23 19:30:15 +0200

    added social login panel

---
+++ src/components/LoginView/SocialLoginPanel.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="social-login-panel">
+    <app-icon-button
+      v-for="(value, key, i) in socials"
+      :key="i"
+      @click="() => handleSocialButton(key)"
+    >
+      <component :is="value" />
+    </app-icon-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppIconButton from '@/components/general/AppIconButton.vue'
+import SocialGoogleIcon from '@/components/icons/SocialGoogleIcon.vue'
+import SocialFacebookIcon from '@/components/icons/SocialFacebookIcon.vue'
+import SocialAppleIcon from '@/components/icons/SocialAppleIcon.vue'
+
+const socials = {
+  google: SocialGoogleIcon,
+  facebook: SocialFacebookIcon,
+  apple: SocialAppleIcon
+}
+
+function handleSocialButton(platform: string): void {
+  console.log(`so you're lazy, aren't you? Go on then, use your ${platform} instead`)
+}
+</script>
+
+<style scoped>
+.social-login-panel {
+  display: flex;
+  align-items: center;
+}
+</style>
