adbf90e7026db9df7267bd423b7b152a2c61f72c
Stanislav Korneev <korstasvik@gmail.com>
2024-04-23 13:17:01 +0200

    introduce basic auth store

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,4 +1,25 @@
 import { defineStore } from 'pinia'
 
-export const useAuthStore = defineStore('auth', () => {
+interface IState {
+  isLoggedIn: boolean,
+}
+
+export const useAuthStore = defineStore('auth', {
+  state: (): IState => ({
+    isLoggedIn: JSON.parse(localStorage.getItem('isLoggedIn') ?? 'false'),
+  }),
+
+  actions: {
+    login(): void {
+      this.isLoggedIn = true;
+      this.syncLocalStorage();
+    },
+    logout(): void {
+      this.isLoggedIn = false;
+      this.syncLocalStorage();
+    },
+    syncLocalStorage(): void {
+      localStorage.setItem('isLoggedIn', JSON.stringify(this.isLoggedIn));
+    }
+  }
 })
