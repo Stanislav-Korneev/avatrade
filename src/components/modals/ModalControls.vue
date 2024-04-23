02a244ced11100cd4a3b9fdf6cda681574fc5542
Stanislav Korneev <korstasvik@gmail.com>
2024-04-23 16:21:17 +0200

    added modal control panel

---
+++ src/components/modals/ModalControls.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="modal-controls">
+    <app-icon-button @click="handleSupportButton" class="support-button">
+      <support-icon />
+    </app-icon-button>
+    <app-icon-button @click="handleCloseButton" class="close-button">
+      <close-icon />
+    </app-icon-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppIconButton from '@/components/general/AppIconButton.vue'
+import SupportIcon from '@/components/icons/SupportIcon.vue'
+import CloseIcon from '@/components/icons/CloseIcon.vue'
+
+function handleSupportButton() {
+  console.log('so, you want to talk to smb, huh?')
+}
+
+function handleCloseButton() {
+  console.log('so, you want to close modal, huh?')
+}
+
+</script>
+
+<style scoped>
+.modal-controls {
+  display: flex;
+  align-items: center;
+}
+
+.support-button {
+  width: 32px;
+  height: 28px;
+  color: black
+}
+
+.close-button {
+  margin-left: 14px;
+  width: 24px;
+  height: 24px;
+  color: black
+}
+</style>
