5348260cf029230547e4885b6c7e54eb83b843aa
Stanislav Korneev <korstasvik@gmail.com>
2024-04-23 16:20:05 +0200

    added app-icon-button

---
+++ src/components/general/AppIconButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+
+button:active {
+  transform: scale(.94);
+}
+</style>
