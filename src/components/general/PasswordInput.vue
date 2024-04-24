897fc93e7e9607f2ad46223f1ce80c9f6abfa171
Stanislav Korneev <korstasvik@gmail.com>
2024-04-24 15:36:59 +0200

    adds password input

---
+++ src/components/general/PasswordInput.vue
@@ -0,0 +1,48 @@
+<template>
+  <app-basic-input
+    :type
+    :id
+    :placeholder
+    :validator
+    @button-click="handleHidePassword"
+  >
+    <template v-slot:icon>
+      <password-icon />
+    </template>
+    <template v-slot:button>
+      <app-icon-button type="button" @click="handleHidePassword">
+        <hide-password-icon />
+      </app-icon-button>
+    </template>
+  </app-basic-input>
+</template>
+
+<script setup lang="ts">
+import AppBasicInput from '@/components/general/AppBasicInput.vue'
+import PasswordIcon from '@/components/icons/PasswordIcon.vue'
+import HidePasswordIcon from '@/components/icons/HidePasswordIcon.vue'
+import { ref } from 'vue'
+import AppIconButton from '@/components/general/AppIconButton.vue'
+
+defineProps({
+  id: {
+    type: String,
+    default: 'password',
+  },
+  placeholder: {
+    type: String,
+    default: 'Password',
+  },
+  validator: {
+    type: Function,
+    default: () => ''
+  },
+})
+
+const type = ref<string>('password');
+
+function handleHidePassword(): void {
+  type.value = type.value === 'password' ? 'text' : 'password';
+}
+
+</script>
